9970264dcc8e55b7662728958d522b1824a875bc
maxmethods <maxmethodss@gmail.com>
2025-11-06 22:09:35 +0100

    scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -3,14 +3,14 @@ export default async function handler(req, res) {
   if (!url) return res.status(400).json({ error: "Brak URL" });
 
   try {
-    // Pobranie HTML strony
-    const response = await fetch(url);
-    const html = await response.text();
-
-    // Dodanie Twojego linku afiliacyjnego
+    // Dodaj Twój link afiliacyjny
     const finalLink = url.includes("affcode=")
       ? url
       : url + (url.includes("?") ? "&" : "?") + "affcode=dexxter";
+
+    // Pobranie HTML strony
+    const response = await fetch(url, { headers: { 'User-Agent': 'Mozilla/5.0' } });
+    const html = await response.text();
 
     // Zwracamy HTML i finalLink w JSON
     res.status(200).json({ html, finalLink });
